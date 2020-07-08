f8c588262b533cb81cf412927475fa846a64ca61
Dillon Larson <dillon.larson@sjsu.edu>
2020-07-08 12:26:09 -0700

    Deployed to firebase hosting

--- src/app/models/dictionary.ts
+++ src/app/models/dictionary.ts
@@ -3,20 +3,11 @@ import { Views } from './views.enum';
 import { Word } from './word';
 
 export class Dictionary {
-    constructor() {
-        this.name = '';
-        this.owner = null;
-        this.admins = [];
-        this.moderators = [];
-        this.view = Views.Public;
-        this.tags = [];
-        this.words = [];
-	}
-    public name: string;
-    public owner: User;
-	public admins: User[];
-    public moderators: User[];
-    public view: Views;
-    public tags: string[];
-    public words: Word[];
+    name: string;
+    owner: User;
+	admins: User[];
+    moderators: User[];
+    view: Views;
+    tags: string[];
+    words: Word[];
 }
