51fd49e90313177cf485b664538cea28ce65744c
dill-larson <dillonlouislarson@gmail.com>
2020-07-22 18:01:01 -0700

    Changed library from string[] to Dictionary[]

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,9 @@
+import { Dictionary } from './dictionary';
+
 export interface User {
 	id: string;
 	name?: string;
 	email: string;
 	password?: string;
-	library?: string[]; //dictionary IDs
+	library?: Dictionary[];
 }
