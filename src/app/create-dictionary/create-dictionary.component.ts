aca6b133e7e203d0b0531061aea274e43a8303cc
dill-larson <dillonlouislarson@gmail.com>
2020-07-22 18:00:28 -0700

    Added error handling that alerts user

--- src/app/create-dictionary/create-dictionary.component.ts
+++ src/app/create-dictionary/create-dictionary.component.ts
@@ -1,70 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { DictionaryService } from '../services/dictionary.service';
 import { UserService } from '../services/user.service';
 import { Dictionary } from '../models/dictionary';
 import { User } from '../models/user';
+import { Error } from '../models/error';
 import { Views } from '../models/views.enum';
-
 
 @Component({
   selector: 'app-create-dictionary',
   templateUrl: './create-dictionary.component.html',
   styleUrls: ['./create-dictionary.component.css']
 })
-export class CreateDictionaryComponent implements OnInit {
+export class CreateDictionaryComponent implements OnInit, OnDestroy {
   public dictionary: Dictionary;
+  public error: Error;
   public user: User;
+  private userSubscription: Subscription;
 
-  constructor(private router: Router, private dictionaryService: DictionaryService, private userService: UserService) {
+  constructor(private router: Router, private dictionaryService: DictionaryService, public userService: UserService) {
+    this.error = {
+      code: '',
+      message: ''
+    };
     this.user = {
       id: '',
       name: '',
       email: '',
       password: '',
       library: []
-    }
+    };
     this.dictionary = {
       name: '',
       owner: this.user.id,
       view: Views.Public,
       tags: []
-    }
+    };
   }
 
   ngOnInit(): void {
-    this.getUser();
+    this.userSubscription = this.userService.user$.subscribe(user => {
+      this.user = user
+      this.dictionary.owner = user.id;
+    });
   }
 
-  getUser(): void {
-    this.userService.currentUser.subscribe(user => {
-      if(user != null) {
-        this.user = user
-        this.dictionary.owner = user.id;
-      }
-    });
-    // if(localStorage.getItem("token") != '') {
-    //   this.user.id = localStorage.getItem("token");
-    // }
-    // else {
-    //   this.user.id = sessionStorage.getItem("token");
-    // }
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 
   addDictionary() {
   	if(this.dictionary.name) {
       if(this.user.id != "") {
-        console.log(this.dictionary);
-        this.dictionaryService.addDictionary(this.dictionary);
-        //this.router.navigate(['/dictionary', this.dict.name]); TODO: How to get dictionary ID (maybe create it myself?)
-      }
-      else {
-        alert('You need to be logged in to create a dictionary')
+        this.dictionaryService.addDictionary(this.dictionary).then(document => {
+          this.dictionary.id = document.id
+          this.router.navigate(['/dictionary', this.dictionary.id]);
+        });
       }
   	} else {
-  		alert('Dictionary name is required');
+      this.error.code = "Missing Name";
+      this.error.message = "Dictionary creation requires a name to be specified.";
   	}
+  }
+
+  closeError() {
+    this.error.code = '';
+    this.error.message = '';
   }
 
 }
