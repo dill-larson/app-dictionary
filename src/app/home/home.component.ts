094397c9ae4ef19fba9f511f951fba41aa77fd90
dill-larson <dillonlouislarson@gmail.com>
2020-07-27 14:11:17 -0700

    ✨ added cards to display user dictionaries

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { DictionaryService } from '../services/dictionary.service';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  public user: User;
+  private userSubscription: Subscription;
+  private dictionarySubscrition: Subscription;
+  public error: Error;
+  public length: Array<number> = new Array<number>();
 
-  constructor() { }
+  constructor(private userService: UserService, private dictionaryService: DictionaryService) {
+    this.user = {
+      id: '',
+      name: '',
+      email: '',
+      password: '',
+      library: []
+    };
+  }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.userSubscription = this.userService.user$.subscribe(user => {
+      this.user = user;
+      this.getUserLibrary(this.user);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+    this.dictionarySubscrition.unsubscribe();
+  }
+
+  getUserLibrary(user: User) {
+    if(user?.id != null) {
+      this.dictionarySubscrition = this.dictionaryService.getDictionaries(user.id).subscribe(dictionaries => {
+        this.user.library = dictionaries;
+        //TODO: Find a better solution of displaying cards
+        this.length = new Array<number>(Math.ceil(dictionaries.length/4));
+        this.length.fill(0); //Used in nested forLoop in displaying cards
+      });
+    }
+  }
 
 }
